93ecbe994543f5cb9846ff9dd63931ba911ca49e
Mateusz Grotek <unoduetre@poczta.onet.pl>
2014-03-16 13:39:17 +0100

    pierdoła

--- src/xml/XMLHandler.java
+++ src/xml/XMLHandler.java
@@ -22,13 +22,13 @@ public class XMLHandler extends DefaultHandler
   @Override
   public void startElement(String uri, String localName, String qName, Attributes attributes)
   {
-    System.out.println("<"+localName+">");
+    System.out.print("<"+localName+">");
   }
 
   @Override
   public void endElement(String uri, String localName, String qName) 
   {
-    System.out.println("</"+localName+">");
+    System.out.print("</"+localName+">");
   }
 
   @Override
